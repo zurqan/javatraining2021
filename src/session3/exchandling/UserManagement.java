9a1c186e412f6ed93352869ff8b4511bcb35ecc3
malzurqan <zurqan@gmail.com>
2021-04-07 18:53:12 +0300

    Session 3

---
+++ src/session3/exchandling/UserManagement.java
@@ -0,0 +1,14 @@
+package session3.exchandling;
+
+import session3.sloid.ocp.User;
+
+public class UserManagement {
+
+    public String save(User user){
+        //
+        //if id exist -> Throw USerAlreadyExistException
+        //if DB connection down -> DBConnectionException
+        //If Missing email -> Throw MissingEmailException
+        return "";
+    }
+}
