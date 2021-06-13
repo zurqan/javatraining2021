793c590aa1e521c0d64bbaeb90f48ced5caff83a
malzurqan <zurqan@gmail.com>
2021-06-13 13:56:39 +0300

    Session 9

--- src/session8ds/MyLinkedList.java
+++ src/session8ds/MyLinkedList.java
@@ -3,6 +3,7 @@ package session8ds;
 import session5.step1.CBiFunction;
 import session6.RecApplication.TailCall;
 
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Optional;
 import java.util.StringJoiner;
@@ -40,33 +41,33 @@ public class MyLinkedList<E> {
         }
     }
 
-    public Optional<E> first(){
+    public Optional<E> first() {
 
         return first == null
                 ? Optional.empty()
                 : Optional.of(first.data);
     }
 
-    public Optional<E> last(){
+    public Optional<E> last() {
 
-        return last ==null
-                ?Optional.empty()
-                :Optional.of(last.data);
+        return last == null
+                ? Optional.empty()
+                : Optional.of(last.data);
     }
 
-    public MyLinkedList<E> push(E e){
+    public MyLinkedList<E> push(E e) {
         addFirst(e);
 
         return this;
     }
 
-    public MyLinkedList<E> add(E e){
+    public MyLinkedList<E> add(E e) {
         addLast(e);
         return this;
     }
 
-    public Optional<E> get(int index){
-        if(index>=size|| index<0){
+    public Optional<E> get(int index) {
+        if (index >= size || index < 0) {
             return Optional.empty();
         }
 
@@ -75,45 +76,45 @@ public class MyLinkedList<E> {
     }
 
     private Node node(int index) {
-        Node nav= first;
+        Node nav = first;
         for (int i = 0; i < index; i++) {
-            nav=nav.next;
+            nav = nav.next;
         }
         return nav;
     }
 
     public MyLinkedList<E> addFirst(E e) {
         Node oldFirstNode = first;
-        Node addedNode = new Node(null,oldFirstNode,e);
+        Node addedNode = new Node(null, oldFirstNode, e);
 
-        first=addedNode;
+        first = addedNode;
 
-        if(oldFirstNode == null){
-            last=addedNode;
-        }else{
-            oldFirstNode.previous=addedNode;
+        if (oldFirstNode == null) {
+            last = addedNode;
+        } else {
+            oldFirstNode.previous = addedNode;
         }
         size++;
         return this;
     }
 
-    public MyLinkedList<E>  addLast(E e){
+    public MyLinkedList<E> addLast(E e) {
         Node oldLast = last;
         Node newLastNode = new Node(oldLast, null, e);
-        last=newLastNode;
-        if(oldLast==null){
-            first=newLastNode;
-        }else{
-            oldLast.next=newLastNode;
+        last = newLastNode;
+        if (oldLast == null) {
+            first = newLastNode;
+        } else {
+            oldLast.next = newLastNode;
         }
 
         size++;
         return this;
     }
 
-    public MyLinkedList<E> addAll(MyLinkedList<? extends E> another){
+    public MyLinkedList<E> addAll(MyLinkedList<? extends E> another) {
         Iterator<? extends E> iterate = another.iterate();
-        while (iterate.hasNext()){
+        while (iterate.hasNext()) {
             E next = iterate.next();
             add(next);
         }
@@ -121,13 +122,13 @@ public class MyLinkedList<E> {
     }
 
 
-    public <U> U reduceL(U seed, BiFunction<?super U,?super E,?extends U> accFunction){
+    public <U> U reduceL(U seed, BiFunction<? super U, ? super E, ? extends U> accFunction) {
 //        return reduceL(seed,acc->e->accFunction.apply(acc,e));
         return reduceL(seed, currying(accFunction));
     }
 
-//a->b->a
-    public <U> U reduceL(U seed, CBiFunction<?super U,?super E,?extends U> accFunction){
+    //a->b->a
+    public <U> U reduceL(U seed, CBiFunction<? super U, ? super E, ? extends U> accFunction) {
 
         U result = seed;
 
@@ -139,7 +140,7 @@ public class MyLinkedList<E> {
         return result;
     }
 
-    public <U> U reduceR(U seed,BiFunction<?super E,?super U,?extends U> accFunction){
+    public <U> U reduceR(U seed, BiFunction<? super E, ? super U, ? extends U> accFunction) {
 
         return reduceR(seed, currying(accFunction));
 
@@ -147,12 +148,12 @@ public class MyLinkedList<E> {
 
     private <U> U reduceR(U seed, CBiFunction<? super E, ? super U, ? extends U> accFunction) {
 //        return reduceR(seed,accFunction,last);
-        return reduceR(seed,accFunction,last).eval();
+        return reduceR(seed, accFunction, last).eval();
     }
 
-    public <U> U reduceR(U seed,BiFunction<?super E,?super U,?extends U> accFunction,BiFunction<E,U,Boolean> stopCondition){
+    public <U> U reduceR(U seed, BiFunction<? super E, ? super U, ? extends U> accFunction, BiFunction<E, U, Boolean> stopCondition) {
 
-        return reduceR(seed, currying(accFunction),currying(stopCondition));
+        return reduceR(seed, currying(accFunction), currying(stopCondition));
 
     }
 
@@ -163,31 +164,35 @@ public class MyLinkedList<E> {
 //                : reduceR(accFunction.apply(node.data).apply(acc), accFunction, node.previous);
 //    }
 
-    private <U> TailCall<U> reduceR(U acc, CBiFunction<? super E,? super U,? extends U> accFunction, Node node) {
+    private <U> TailCall<U> reduceR(U acc, CBiFunction<? super E, ? super U, ? extends U> accFunction, Node node) {
 
         return node == null
                 ? result(acc)
-                : suspend(()->reduceR(accFunction.apply(node.data).apply(acc), accFunction, node.previous));
+                : suspend(() -> reduceR(accFunction.apply(node.data).apply(acc), accFunction, node.previous));
     }
 
     private <U, V> U reduceR(U seed, CBiFunction<? super E, ? super U, ? extends U> accFunction, CBiFunction<E, U, Boolean> stopCondition) {
 
-        return reduceR(seed,  accFunction, last, stopCondition).eval();
+        return reduceR(seed, accFunction, last, stopCondition).eval();
 
     }
 
-    private <U> TailCall<U> reduceR(U acc, CBiFunction<? super E,? super U,? extends U> accFunction, Node node, CBiFunction<E, U, Boolean> stopCondition) {
+    private <U> TailCall<U> reduceR(U acc, CBiFunction<? super E, ? super U, ? extends U> accFunction, Node node, CBiFunction<E, U, Boolean> stopCondition) {
 
-        return node == null || stopCondition.apply(node.data).apply(acc)
+        U newAccValue = node == null?null:accFunction.apply(node.data).apply(acc);
+
+        return node == null
                 ? result(acc)
-                : suspend(()->reduceR(accFunction.apply(node.data).apply(acc), accFunction, node.previous));
+                : stopCondition.apply(node.data).apply(newAccValue)
+                ? result(newAccValue)
+                : suspend(() -> reduceR(newAccValue, accFunction, node.previous));
     }
 
-    public MyLinkedList<E> filter(Predicate<? super E> filter){
+    public MyLinkedList<E> filter(Predicate<? super E> filter) {
         return reduceL(new MyLinkedList<>(), acc -> e -> filter.test(e) ? acc.addLast(e) : acc);
     }
 
-    public boolean allMatch(Predicate<? super E> condition){
+    public boolean allMatch(Predicate<? super E> condition) {
 
 //        if(isEmpty()){
 //            return false;
@@ -204,19 +209,30 @@ public class MyLinkedList<E> {
 //                ?false
 //                :reduceR(true,e->acc->acc&&condition.test(e));//iterate over all element inside the list
         return isEmpty()
-                ?false
-                :reduceR(true,e->acc->acc&&condition.test(e),e->acc->!acc);
+                ? false
+                : reduceR(true, e -> acc -> acc && condition.test(e), e -> acc -> !acc);
     }
 
-    public boolean noneMatch(Predicate<? super E> condition){
+    public boolean noneMatch(Predicate<? super E> condition) {
+        return allMatch(condition.negate());
+    }
 
+    public boolean anyMatch(Predicate<? super E> condition) {
+
+        return reduceR(false, e -> acc -> condition.test(e), e -> acc -> acc);
+    }
+
+    public Optional<E> min(Comparator<E> comparator){
+
+
+        return Optional.empty();
     }
 
     private boolean isEmpty() {
-        return size==0;
+        return size == 0;
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
@@ -225,25 +241,25 @@ public class MyLinkedList<E> {
     public String toString() {
         //[e1,e2,e3]
         return reduceL(
-                new StringJoiner(",","[","]"),
-                (acc, e)->acc.add(e.toString()))
+                new StringJoiner(",", "[", "]"),
+                (acc, e) -> acc.add(e.toString()))
                 .toString();
     }
 
-    public MyLinkedList<E> reversed(){
-        return reduceR(new MyLinkedList<>(),(e->acc->acc.addLast(e)));
+    public MyLinkedList<E> reversed() {
+        return reduceR(new MyLinkedList<>(), (e -> acc -> acc.addLast(e)));
     }
 
-    public <U> MyLinkedList<U> map(Function<?super E,?extends U> mapFunction){
+    public <U> MyLinkedList<U> map(Function<? super E, ? extends U> mapFunction) {
 
-        return reduceR(new MyLinkedList<>(),e->acc->acc.push(mapFunction.apply(e)));
+        return reduceR(new MyLinkedList<>(), e -> acc -> acc.push(mapFunction.apply(e)));
     }
 
-    public <U> MyLinkedList<U> flatMap(Function<?super E,MyLinkedList<? extends U>> flatMap){
-        return reduceL(new MyLinkedList<>(),acc->e->acc.addAll(flatMap.apply(e)));
+    public <U> MyLinkedList<U> flatMap(Function<? super E, MyLinkedList<? extends U>> flatMap) {
+        return reduceL(new MyLinkedList<>(), acc -> e -> acc.addAll(flatMap.apply(e)));
     }
 
-    public Iterator<E> iterate(){
+    public Iterator<E> iterate() {
         return new MyLinkedListIterator(first);
     }
 
@@ -258,13 +274,13 @@ public class MyLinkedList<E> {
 
         @Override
         public boolean hasNext() {
-            return currentNode!=null;
+            return currentNode != null;
         }
 
         @Override
         public E next() {
-            lastAccessedNode= currentNode;
-            currentNode=currentNode.next;
+            lastAccessedNode = currentNode;
+            currentNode = currentNode.next;
             return lastAccessedNode.data;
         }
     }
@@ -303,7 +319,7 @@ public class MyLinkedList<E> {
         System.out.println("sum = " + sum);
 
 
-        String reduce = another.reduceR("", e -> acc -> acc+e);//
+        String reduce = another.reduceR("", e -> acc -> acc + e);//
         System.out.println("reduce = " + reduce);
 
         System.out.println("another = " + another);
@@ -319,7 +335,7 @@ public class MyLinkedList<E> {
         MyLinkedList<String> strMap = another.map(e -> "Number is " + e);
         System.out.println("strMap = " + strMap);
 
-        IntStream.rangeClosed(1,3).map(a->a+10).forEach(System.out::println);
+        IntStream.rangeClosed(1, 3).map(a -> a + 10).forEach(System.out::println);
 
 
         MyLinkedList<Object> flattedList = another.flatMap(e -> {
@@ -329,7 +345,6 @@ public class MyLinkedList<E> {
             flatted.push(e - 1);
             return flatted;
         });
-
 
 
         //321 -> <2 3 4> <1 2 3> <0 1 2>
@@ -354,11 +369,21 @@ public class MyLinkedList<E> {
 
         System.out.println("another.allMatch(e->e<100) = " + another.allMatch(e -> e < 100));
 
+        System.out.println("another.noneMatch(isEven) = " + another.noneMatch(isEven));
+        System.out.println("another.noneMatch(isEven.negate()) = " + another.noneMatch(isEven.negate()));
+
+        System.out.println("another.noneMatch(e->e>100) = " + another.noneMatch(e -> e > 100));
+
+
+        System.out.println("another.anyMatch(isEven) = " + another.anyMatch(isEven));
+        System.out.println("another.anyMatch(isEven.negate()) = " + another.anyMatch(isEven.negate()));
+        System.out.println("another.anyMatch(e->e>100) = " + another.anyMatch(e -> e > 100));
+
     }
 
     private static void printList(MyLinkedList<Integer> numbers) {
         Iterator<Integer> iterate = numbers.iterate();
-        while (iterate.hasNext()){
+        while (iterate.hasNext()) {
             Integer next = iterate.next();
             System.out.println("next = " + next);
         }
