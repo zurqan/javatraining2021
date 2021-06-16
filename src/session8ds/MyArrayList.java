d7cc81e8082a2bbef4bfcdfe710a0db5e63deac9
malzurqan <zurqan@gmail.com>
2021-06-16 13:46:00 +0300

    Session 10

--- src/session8ds/MyArrayList.java
+++ src/session8ds/MyArrayList.java
@@ -1,11 +1,12 @@
 package session8ds;
 
 import session5.step1.CBiFunction;
+import session6.Application.Tuple;
 import session6.RecApplication.TailCall;
 
-import java.util.Arrays;
-import java.util.Optional;
-import java.util.StringJoiner;
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 import static session6.RecApplication.TailCall.result;
 import static session6.RecApplication.TailCall.suspend;
@@ -104,6 +105,10 @@ public class MyArrayList<E> {
 
     }
 
+    public Iterator<E> iterate(){
+        return new MyArrayListIterator(0);
+    }
+
     public <U> MyArrayList<U> scanL(U seed,CBiFunction<U,E,U> accFunction){
         return reduceL(new MyArrayList<U>().addFirst(seed),acc->e->acc.addLast(accFunction.apply((U)acc.getLast().get()).apply(e)));
     }
@@ -111,6 +116,100 @@ public class MyArrayList<E> {
     public <U> MyArrayList<U> scanR(U seed,CBiFunction<E,U,U> accFunction){
 
         return reduceR(new MyArrayList<U>().addLast(seed),e->acc->acc.addFirst(accFunction.apply(e).apply((U)acc.get(0).get())));
+    }
+
+    public boolean contains(Object targetObject){
+        Iterator<E> iterate = iterate();
+        while (iterate.hasNext()) {
+            E element = iterate.next();
+            if(element.equals(targetObject)){
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    public MyArrayList<E> filter(Predicate<? super E> filter){
+        return reduceL(new MyArrayList<>(), acc -> e -> filter.test(e) ? acc.addLast(e) : acc);
+    }
+
+    public MyArrayList<E> addAll(MyArrayList<? extends E> another){
+        // performance issue .. expantion size+another.size if current array less than  total size
+        expandIfNeeded(this.size+another.size);
+        Iterator<? extends E> iterate = another.iterate();
+        while (iterate.hasNext()) {
+            E next = iterate.next();
+            addLast(next);
+        }
+        return this;
+    }
+
+    public <U> MyArrayList<U> map(Function<? super E,?extends U> mapFunction){
+
+        return reduceL(new MyArrayList<>(),acc->e->acc.addLast(mapFunction.apply(e)));
+    }
+
+    public <U> MyArrayList<U> flatMap(Function<? super E,MyArrayList<? extends U>> function){
+        return reduceL(new MyArrayList<>(),acc->e->acc.addAll(function.apply(e)));
+    }
+
+    public <K> Map<K,MyArrayList<E>> groupBy(Function<E,K> keyMapper){
+
+        return reduceR(
+                new HashMap<>(),
+                e->acc->{
+                    acc.compute(keyMapper.apply(e),(k,v)->v==null?new MyArrayList<E>().addFirst(e):v.addFirst(e));
+                    return acc;
+                }
+
+        );
+    }
+
+    public <U> MyArrayList<Tuple<E,U>> zip(MyArrayList<U> another){
+        return null;
+    }
+
+    public Optional<E> min(Comparator<E> comparator){
+
+
+        return isEmpty()
+                ?Optional.empty()
+                :Optional.of(reduceR(get(size-1).get(),e->acc->comparator.compare(e,acc)>0?acc:e));
+    }
+
+    private boolean isEmpty() {
+        return size==0;
+    }
+
+    public Optional<E> max(Comparator<E> comparator){
+
+
+        return isEmpty()
+                ?Optional.empty()
+                :Optional.of(reduceR(get(size-1).get(),e->acc->comparator.compare(e,acc)<0?acc:e));
+    }
+
+    private class MyArrayListIterator implements Iterator<E>{
+
+        private int index = 0;
+
+        public MyArrayListIterator(int index) {
+            this.index = index;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return index<size;
+        }
+
+        @Override
+        public E next() {
+            if(index>=size || index<0){
+                throw new ArrayIndexOutOfBoundsException();
+            }
+            return (E)data[index++];
+        }
     }
 
     @Override
@@ -158,9 +257,42 @@ public class MyArrayList<E> {
 
         System.out.println("tenNumbers.scanR(0,e->acc->e+acc) = " + tenNumbers.scanR(0, e -> acc -> e + acc));
 
+        Iterator<Integer> iterate = tenNumbers.iterate();
+        while (iterate.hasNext()) {
+            Integer next = iterate.next();
+            System.out.println("next = " + next);
+
+        }
         System.out.println("tenNumbers.removeByIndex(5) = " + tenNumbers.removeByIndex(5));
         System.out.println("tenNumbers = " + tenNumbers);
 
 
+        System.out.println("tenNumbers.contains(10) = " + tenNumbers.contains(10));
+        System.out.println("tenNumbers.contains(11) = " + tenNumbers.contains(11));
+
+
+        System.out.println("tenNumbers.filter(e->e%2==0) = " + tenNumbers.filter(e -> e % 2 == 0));
+
+
+        MyArrayList<Integer> myArrayList = tenNumbers
+                .flatMap(e -> MyArrayList.of(10 * e - 1, 10 * e, 10 * e + 1));
+        System.out.println("myArrayList = " + myArrayList);
+
+        MyArrayList<String> mapped = tenNumbers.map(n -> "The number is: " + n);
+        System.out.println("mapped = " + mapped);
+
+        Map<Boolean, MyArrayList<Integer>> booleanMyArrayListMap = tenNumbers.groupBy(e -> e % 2 == 0);
+
+        System.out.println("booleanMyArrayListMap = " + booleanMyArrayListMap);
+
+        MyArrayList<Integer> first = MyArrayList.of(1, 2);
+
+        MyArrayList<Integer> second = MyArrayList.of(3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
+        System.out.println("first.addAll(second) = " + first.addAll(second));
+
+        System.out.println("tenNumbers.min(Comparator.comparing(a->a)) = " + tenNumbers.min(Comparator.comparing(a -> a)));
+
+        System.out.println("tenNumbers.max(Comparator.comparing(a -> a)) = " + tenNumbers.max(Comparator.comparing(a -> a)));
+//
     }
 }
