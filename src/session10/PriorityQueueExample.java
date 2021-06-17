6b83e11efe7281496276f6bee5ae6604eebc7271
malzurqan <zurqan@gmail.com>
2021-06-17 10:15:31 +0300

    Session 11

--- src/session10/PriorityQueueExample.java
+++ src/session10/PriorityQueueExample.java
@@ -47,6 +47,43 @@ public class PriorityQueueExample {
 
         //1,10,2,3,5 -> 1,2,3,5,10 -> 3
         //1,10,2,3 -> 1,2,3,10->2.5
+
+        PriorityQueue<Integer> highQueue = new PriorityQueue<>(Comparator.<Integer>comparingInt(a -> a));
+        PriorityQueue<Integer> lowQueue = new PriorityQueue<>(Comparator.<Integer>comparingInt(a -> a).reversed());
+
+        add(highQueue,lowQueue,10);//10  -> 10
+        add(highQueue,lowQueue,4);//4 10 -> 7
+        add(highQueue,lowQueue,5);//4 5 10 -> 5
+        add(highQueue,lowQueue,12);//4 5 10 12 -> 7.5
+        add(highQueue,lowQueue,3);//3 4 5 12 10 ->5
+        add(highQueue,lowQueue,0);//0 3 4 5 10 12 ->4.5
+        add(highQueue,lowQueue,0);//0 0 3 4 5 12 10 ->4
+    }
+
+    private static void add(PriorityQueue<Integer> highQueue, PriorityQueue<Integer> lowQueue, int number) {
+        if(highQueue.size()==lowQueue.size()){
+            if(highQueue.size()!=0 && lowQueue.peek()>number){
+                highQueue.add(lowQueue.poll());
+                lowQueue.add(number);
+            }else {
+                highQueue.add(number);
+            }
+        }else{
+           highQueue.add(number);
+           lowQueue.add(highQueue.poll());
+        }
+
+        double medianValue = median(highQueue,lowQueue);
+
+        System.out.printf("high queue peek after adding %d is %d\n",number,highQueue.peek());
+        System.out.printf("low queue peek after adding %d is %d\n",number,lowQueue.peek());
+        System.out.printf("Median value is %s\n",medianValue);
+    }
+
+    private static double median(PriorityQueue<Integer> highQueue, PriorityQueue<Integer> lowQueue) {
+        return highQueue.size()==lowQueue.size()
+                ?(highQueue.peek()+lowQueue.peek())/2.0
+                :highQueue.peek();
     }
 
     private static void addNumber(PriorityQueue<Integer> largeFive, int i) {
